88c550eee060059fda7c8ed6e7536e93a888b1d5
pangyu <1510397456@qq.com>
2022-03-14 22:05:12 +0800

    initialize the library.h and library.c(add some functions' names)

---
+++ library.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "library.h"
+#include "book_management.h"
+
+//initialize the library
+//read books from txt
+void initLibrary(BookList* theBook){
+	//
+}
+
+//create a file to store the database of books
+void store_books(FILE* file){
+	
+}
+
+//load books from txt
+void load_books(FILE* file){
+	//
+}
+
+//exit the library
+void exitLibrary(BookList* theBook){
+	//
+}
+
+//the library main menu
+void libraryMenu(void){
+	int libraryOpen = 1;
+
+
+	while (libraryOpen){
+		printf("\t\t\t *============================================*\n");
+		printf("\t\t\t | * - * - * Welcome to Our Library * - * - * |\n");
+	}
+}
+
---
+++ library.h
@@ -0,0 +1,6 @@
+//the function of the library menu
+void libraryMenu(void);
+//initialize the library
+void initLibrary(BookList* theBook);
+//exit the library
+void exitLibrary(BookList* theBook);
