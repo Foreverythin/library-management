10d1d080265117a5e8afeb595e2800dde1bb34dd
pangyu <1510397456@qq.com>
2022-03-16 17:21:53 +0800

    complete the txt-read function in library.c

--- library.c
+++ library.c
@@ -2,33 +2,66 @@
 #include <stdlib.h>
 
 #include "library.h"
-//#include "book_management.h"
+#include "book_management.h"
+
+#define bookFile "books.txt"
 
 //initialize the library
 //read books from txt
-//void initLibrary(BookList* theBook){
-//	//
-//}
+void initLibrary(char* bookFile, BookList* theBook){
+	FILE* file;
+	file = fopen(bookFile, "r");
+	if (file != NULL){
+		theBook.length = boad_books(file);
+	}
+	fclose(file);
+}
 //
 ////create a file to store the database of books
 //void store_books(FILE* file){
 //	
 //}
 //
-////load books from txt
-//void load_books(FILE* file){
-//	//
-//}
+////load books from txt if the book file "books.txt" exist and store them to the book list
+int load_books(FILE* books, BookList* theBook){
+	int numbooks = 0;
+	BookList* tmp;
+
+	while(!feof(books)){
+		tmp = tailInsert(theBook);
+		fscanf(books, "%d %s %s %d %d", &(tmp->id), &(tmp->title), &(tmp->authors), &(tmp->year), &(tmp->copies));
+		numbooks ++;
+	}
+
+	return numbooks;
+}
 //
 ////exit the library
 //void exitLibrary(BookList* theBook){
 //	//
 //}
 //
+
+//insert a node at the tail of the book list and return the pointer pointing the last node
+BookList* tailInsert(BookList* theBook){
+	BookList* tmp = (BookList*)malloc(sizeof(BookList));
+	tmp = theBook;
+	while (tmp->next != NULL){
+		tmp = tmp->next;
+	}
+	BookList* new = (BookList*)malloc(sizeof(BookList));
+	new->next = NULL;
+	tmp->next = new;
+
+	return tmp;
+}
+
 ////the library main menu
 void libraryMenu(void){
 	int libraryOpen = 1;
-
+	BookList* theBook = (BookList*)malloc(sizeof(BookList));
+	theBook->next = NULL;	
+	initLibrary(bookFile, theBook);
 
 	while (libraryOpen){
 		printf(" *============================================*\n");
--- library.h
+++ library.h
@@ -1,6 +1,10 @@
 //the function of the library menu
 void libraryMenu(void);
 //initialize the library
-//void initLibrary(BookList* theBook);
+void initLibrary(BookList* theBook);
 //exit the library
 //void exitLibrary(BookList* theBook);
+//load books from "books.txt" if exists and store them into the book list
+int load_books(FILE* books, BookList* theBook);
+//insert a node at the tail of the book list and return the pointer pointing the last node
+BookList* tailInsert(BookList* theBook);
